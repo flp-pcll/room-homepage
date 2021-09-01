2bc6dc2603e6bc5d3ee73d466341f7a03bd25c20
Felipe Pacelli <fgpacelli1@gmail.com>
2021-09-01 18:48:02 -0300

    added media query in style.css

--- script.js
+++ script.js
@@ -15,12 +15,13 @@ const sliderItems = [
 
     {
         desktopImg: 'images/desktop-image-hero-3.jpg',
-        mobileImg: 'images/mobile-image-hero-2.jpg',
+        mobileImg: 'images/mobile-image-hero-3.jpg',
         title: 'Manufactured with the best materials',
         description: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.`
     }
 ]
 let item = 0;
+let mobileSize = window.matchMedia('(max-width: 1000px)'); //This variable will be used to test the media query to load the correct img file;
 
 console.log(window.innerWidth); //Need to test the width to change img from desktop to mobile;
 document.addEventListener('DOMContentLoaded', (e) => {
@@ -52,9 +53,15 @@ function loadSliderItem(i) {
     const titleArea = document.querySelector('.text-area h1');
     const descriptionArea = document.querySelector('.text-area p');
 
-    imgArea.style.background = `url('${sliderItems[i].desktopImg}')`;
-    imgArea.style.backgroundSize = 'cover';
-    imgArea.style.backgroundPosition = 'center';
+    if(mobileSize.matches) {  
+        imgArea.style.background = `url('${sliderItems[i].mobileImg}')`;
+        imgArea.style.backgroundSize = 'cover';
+        imgArea.style.backgroundPosition = 'center';
+    } else {
+        imgArea.style.background = `url('${sliderItems[i].desktopImg}')`;
+        imgArea.style.backgroundSize = 'cover';
+        imgArea.style.backgroundPosition = 'center';
+    }
 
     titleArea.innerHTML = sliderItems[i].title;
     descriptionArea.innerHTML = sliderItems[i].description;
